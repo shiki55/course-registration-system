19a913f378ae75fcb0686a6f16b0e1c0b5691e56
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-03-12 18:46:01 -0600

    implemented input validation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import shutil
+from time import sleep
 from regie_pkg.UserAuthenticator import UserAuthenticator
 from regie_pkg.REGIEPersonFactory import REGIEPersonFactory
 from regie_pkg.Student import Student
@@ -9,18 +10,26 @@ from regie_pkg.StudentView import (
                                     ViewSchedule,
                                     )
 from regie_pkg.CourseRegistration import CourseRegistration
-from time import sleep
+from regie_pkg.InputValidator import (
+                                        StudentIDInputValidator,
+                                        MenuChoiceInputValidator,
+                                        ValidationExecutor,
+                                        CouseIDInputValidator,
+                                     )
+
 
 def login() -> Student:
     auth = UserAuthenticator()
     person_factory = REGIEPersonFactory()
+    validation_executor = ValidationExecutor(input_validator=StudentIDInputValidator())
     while True:
-        try:
-            student_id_input = int(input("Enter student id: "))
-        except ValueError:
-            print("student id must be an integer. Please try again.")
+        student_id_input = input("Enter student id: ")
+        err_msg, valid_input = validation_executor.validate(input_val=student_id_input)
+        if not valid_input:
+            print(err_msg)
             print("")
             continue
+        student_id_input = int(student_id_input)
         password_input = input("Enter password: ")
         account_type = 'student'
         if auth.authenticate(id=student_id_input, password=password_input, account_type=account_type):
@@ -38,48 +47,75 @@ def main():
     view_trans = ViewTranscript()
     view_sched = ViewSchedule()
     course_reg = CourseRegistration()
-    student: Student = login()
-    print("\033[1mWelcome to your REGIE account!\033[0m".center(shutil.get_terminal_size().columns))
+    course_id_input_validator = CouseIDInputValidator()
+    menu_choice_input_validator = MenuChoiceInputValidator()
+    validation_executor = ValidationExecutor()
     while True:
-        student.view(view_reg_courses)
-        print('---------------------------')
-        print("\033[1mMenu options:\033[0m")
-        print("\t1. View My Academic Transcript")
-        print("\t2. View My Account Restrictions")
-        print("\t3. View My Course Schedule")
-        print("\t4. Add Course Section/Lab")
-        print("\t5. Remove Course Section/Lab")
-        print("\t6. Remove All Course Section/Lab")
-        print("\t7. Change Password")
-        print("")
-        try: 
-            user_choice = int(input("Enter choice by number: "))
+        student: Student = login()
+        print("\033[1mWelcome to your REGIE account!\033[0m".center(shutil.get_terminal_size().columns))
+        while True:
+            student.view(view_reg_courses)
+            print('---------------------------')
+            print("\033[1mMenu options:\033[0m")
+            print("\t1. View My Academic Transcript")
+            print("\t2. View My Account Restrictions")
+            print("\t3. View My Course Schedule")
+            print("\t4. Add Course Section/Lab")
+            print("\t5. Remove Course Section/Lab")
+            print("\t6. Remove All Course Section/Lab")
+            print("\t7. Change Password")
+            print("\t8. Log Out")
             print("")
-        except ValueError:
-            print("Entered choice must be an integer. Please try again.")
+            user_choice = input("Enter choice by number: ")
+            validation_executor.set_validator(input_validator=menu_choice_input_validator)
+            err_msg, valid_input = validation_executor.validate(input_val=user_choice)
+            if not valid_input:
+                print(err_msg)
+                print("")
+                input("\033[1mHit Enter to go back to Menu options\033[0m")
+                continue
             print("")
-            continue
-        if user_choice == 1:
-            student.view(view_trans)
-        elif user_choice == 2:
-            student.view(view_restrictions)
-        elif user_choice == 3:
-            student.view(view_sched)
-        elif user_choice == 4:
-            course_id = int(input("Enter the eight digit course section/lab id: "))
-            course_reg.add_course(student_id=student.id, course_id=course_id)
-        elif user_choice == 5:
-            course_id = int(input("Enter the eight digit course section/lab id: "))
-            course_reg.drop_course(student_id=student.id, course_id=course_id)
-        elif user_choice == 6:
-            course_reg.drop_all(student_id=student.id)
-        elif user_choice == 7:
-            student._change_password()
-        else:
-            print("Invalid choice, try again.")
-        print("")
-        input("\033[1mHit Enter to go back to Menu options\033[0m")
-        print("")
+            user_choice = int(user_choice)
+            if user_choice == 1:
+                student.view(view_trans)
+            elif user_choice == 2:
+                student.view(view_restrictions)
+            elif user_choice == 3:
+                student.view(view_sched)
+            elif user_choice == 4:
+                course_id = input("Enter the eight digit course section/lab id: ")
+                validation_executor.set_validator(input_validator=course_id_input_validator)
+                err_msg, valid_input = validation_executor.validate(input_val=course_id)
+                if not valid_input:
+                    print(err_msg)
+                    print("")
+                    input("\033[1mHit Enter to go back to Menu options\033[0m")
+                    continue
+                course_reg.add_course(student_id=student.id, course_id=int(course_id))
+            elif user_choice == 5:
+                course_id = input("Enter the eight digit course section/lab id: ")
+                validation_executor.set_validator(input_validator=course_id_input_validator)
+                err_msg, valid_input = validation_executor.validate(input_val=course_id)
+                if not valid_input:
+                    print(err_msg)
+                    print("")
+                    input("\033[1mHit Enter to go back to Menu options\033[0m")
+                    continue
+                course_reg.drop_course(student_id=student.id, course_id=int(course_id))
+            elif user_choice == 6:
+                course_reg.drop_all(student_id=student.id)
+            elif user_choice == 7:
+                student._change_password()
+            elif user_choice == 8:
+                print("Logging off")
+                for _ in range(3):
+                    print("")
+                    sleep(0.75)
+                break
+        
+            print("")
+            input("\033[1mHit Enter to go back to Menu options\033[0m")
+            print("")
 
 
 
