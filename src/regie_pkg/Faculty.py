8d08ba0f75efc332c291bbd77588013ba09cc065
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-12-20 20:36:43 -0600

    renamed Faculty.py to faculty.py & added docstrings

--- src/regie_pkg/Faculty.py
+++ src/regie_pkg/Faculty.py
@@ -1,17 +1,20 @@
+"""This module contains the `Faculty` class."""
+
 from .REGIEPerson import REGIEPerson
 
 class Faculty(REGIEPerson):
-    def __init__(self, 
-                name=None, 
-                status=None, 
-                email=None, 
-                id=None, 
+    """This class represents a faculty member."""
+    def __init__(self,
+                name=None,
+                status=None,
+                email=None,
+                id=None,
                 title=None,
                 ):
-        super().__init__(name=name, 
-                        status=status, 
-                        department="", 
-                        email=email, 
+        super().__init__(name=name,
+                        status=status,
+                        department="",
+                        email=email,
                         id=id,
                         )
         self.title = title
