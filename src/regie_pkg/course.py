f08f60c6ddfcb3bfb6e9e9e5f09c12fb5704e88a
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-12-20 19:58:48 -0600

    added docstring to course.py

--- src/regie_pkg/course.py
+++ src/regie_pkg/course.py
@@ -1,13 +1,25 @@
+"""
+This module contains classes for representing courses and completed courses.
+
+The Course class represents a course, including its ID, name, approval requirements,
+instructor/faculty id, course description, subject area, and division.
+
+The CompletedCourse class represents a course that has been completed
+by a student, including the student's id, grade, year, and quarter in which
+the course was completed.
+"""
 
 class Course:
-    def __init__(self, 
+    """A class representing a course."""
+
+    def __init__(self,
                 _id,
                 name,
                 approval_req,
                 faculty_id,
                 desc,
                 subject,
-                division,                
+                division,
         ):
         self.id=_id
         self.name=name
@@ -19,18 +31,20 @@ class Course:
 
 
 class CompletedCourse(Course):
-    def __init__(self, 
+    """A class representing a course that has been completed by a student."""
+
+    def __init__(self,
                 _id,
                 name,
                 approval_req,
                 faculty_id,
                 desc,
                 subject,
-                division,  
+                division,
                 student_id,
                 grade,
                 year,
-                quarter,             
+                quarter,
         ):
         super().__init__(
                 _id,
@@ -45,26 +59,3 @@ class CompletedCourse(Course):
         self.grade = grade
         self.year = year
         self.quarter = quarter
-
-
-# class ActiveCourse(Course):
-#     def __init__(self, 
-#                 _id,
-#                 name,
-#                 approval_req,
-#                 faculty_id,
-#                 desc,
-#                 subject,
-#                 division,
-#                 reg_status,       
-#         ):
-#         super().__init__(
-#                 _id,
-#                 name,
-#                 approval_req,
-#                 faculty_id,
-#                 desc,
-#                 subject,
-#                 division,
-#         )
-#         self.reg_status = reg_status
