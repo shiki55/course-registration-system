bd04cb563479be21b2c7617f86799d9512c5c86f
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-12-21 15:03:21 -0600

    renamed Student.py & added docstrings

--- src/regie_pkg/Student.py
+++ src/regie_pkg/Student.py
@@ -1,13 +1,22 @@
-from .REGIEPerson import REGIEPerson
+"""This module contains the Student class."""
+
+from .regie_person import REGIEPerson
+from .StudentView import StudentView
 
 class Student(REGIEPerson):
-    def __init__(self, 
-                name=None, 
-                status=None, 
-                department=None, 
-                division=None, 
-                email=None, 
-                id=None, 
+    """
+    A class representing a student in the registration system.
+
+    Inherits from the REGIEPerson class and adds additional attributes specific to students,
+    such as major, division, and restrictions.
+    """
+    def __init__(self,
+                name=None,
+                status=None,
+                department=None,
+                division=None,
+                email=None,
+                id=None,
                 major=None,
                 restrictions=None,
                 ):
@@ -15,10 +24,18 @@ class Student(REGIEPerson):
         self.major = major
         self.division = division
         self.restrictions=restrictions
-      
-    def view(self, StudentView):
-        '''strategy pattern (this implementation is sort of like visitor pattern in some ways)'''
-        StudentView.view(self.id)
-        return 
 
+    def view(self, student_view: StudentView):
+        """
+        Apply a student view strategy to view the student's information (e.g. transcript, schedule, etc).
 
+        This method uses the strategy pattern to view different kinds of student information.
+
+        Args:
+        student_view (StudentView): A class that defines a view strategy for the student.
+
+        Returns:
+        None
+        """
+        student_view.view(self.id)
+        return
