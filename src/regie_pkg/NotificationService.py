816b7fcb42b9fcc6f6774793cb0e176f6ca7cabb
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-12-21 13:50:45 -0600

    delete files

--- src/regie_pkg/NotificationService.py
+++
@@ -1,9 +0,0 @@
-from .Email import NotificationStrategy
-
-
-class NotificationService:
-    '''strategy design pattern'''
-    def __init__(self, NotificationStrategy: NotificationStrategy=None):
-        self.notif_strategy = NotificationStrategy
-    def send_notification(self):
-        return self.notif_strategy.send()
