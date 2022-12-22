d21693d24c3a3425df72088bbe99019aa965d0f0
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-12-21 18:29:12 -0600

    added docstrings to drop_all_db.py

--- src/drop_all_db.py
+++ src/drop_all_db.py
@@ -1,11 +1,12 @@
-from regie_pkg.MySQLConnect import MySQLConnect
-from pymongo import MongoClient
+"""
+Drop the REGIE_db MySQL database and the password_db MongoDB database.
+"""
 
-# dropping all databases
+from regie_pkg.mysql_connect import MySQLConnect
+from regie_pkg.get_mongo_client import get_mongo_client
 
 my_sql_conn = MySQLConnect()
-my_sql_conn.execute_query('DROP DATABASE IF EXISTS REGIE_db;')
+my_sql_conn.execute_query('DROP DATABASE IF EXISTS REGIE_db;', database=None)
 
-
-mongo_client = MongoClient()
-mongo_client.drop_database('password_db') 
+mongo_client = get_mongo_client()
+mongo_client.drop_database('password_db')
