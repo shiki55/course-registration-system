5430bec151059958722223638fe0d820a0382f32
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-12-21 18:22:03 -0600

    added docstring to build_mysql_db.py

--- src/build_mysql_db.py
+++ src/build_mysql_db.py
@@ -1,12 +1,25 @@
-from regie_pkg.MySQLConnect import MySQLConnect
+"""
+Build a MySQL database for the REGIE application.
+
+This script does the following:
+1. Connects to a MySQL server using the MySQLConnect class from the regie_pkg package.
+2. Drops the REGIE_db database if it exists.
+3. Creates the REGIE_db database.
+4. Executes the SQL statements in the 'create_table.sql' file to
+   create the tables in the REGIE_db database.
+5. Executes the SQL statements in the 'populate.sql' file to populate the
+   tables in the REGIE_db database.
+
+Note:
+- The MySQL server must be running and accessible.
+"""
 from time import sleep
 
-'''build mysql db'''
-
+from regie_pkg.mysql_connect import MySQLConnect
 
 mysql_conn = MySQLConnect()
 mysql_conn.execute_query('DROP DATABASE IF EXISTS REGIE_db;', database=None)
-mysql_conn.execute_query('CREATE DATABASE REGIE_db;', database=None) 
+mysql_conn.execute_query('CREATE DATABASE REGIE_db;', database=None)
 
 sleep(1)
 
@@ -19,6 +32,3 @@ sleep(2)
 # populate tables
 with open('../sql_files/populate.sql', encoding='utf-8') as f:
     mysql_conn.execute_query(f.read(), multi=True, commit=True)
-
-
-
