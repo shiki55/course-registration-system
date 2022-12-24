a79992255247fe79e255e796751b44837377e108
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-12-24 12:10:24 +0900

    clean up notification service test

--- src/tests/test_NotificationService.py
+++ src/tests/test_NotificationService.py
@@ -1,16 +1,18 @@
 import os, sys, inspect
-import unittest
 ##
 curr_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(curr_dir)
 sys.path.append(parent_dir)
 ##
-from regie_pkg.student import Student
 from regie_pkg.notification_service import NotificationService
 from regie_pkg.email import Email
 
-class TestNotificationService(unittest.TestCase):
-    def setUp(self): # ran before each method w/ prefix "test_"
+class TestNotificationService:
+    """A class for testing the NotificationService and Email classes"""
+
+    def test_notification_service(self):
+        """Test the send_notification method of the NotificationService class with email."""
+
         email = Email(
             sender="bisping@uchicago.edu",
             to="smith@uchicago.edu",
@@ -18,9 +20,7 @@ class TestNotificationService(unittest.TestCase):
             subject="approval request",
             body="I am requesting an approval to enroll in CS123"
         )
-        self.notif_service1 = NotificationService(notification_strategy=email)
-
-    def test_notification_service(self):
+        notif_service =  NotificationService(notification_strategy=email)
         expected = {
             "sender": "bisping@uchicago.edu",
             "to":"smith@uchicago.edu",
@@ -28,8 +28,4 @@ class TestNotificationService(unittest.TestCase):
             "subject":"approval request",
             "body":"I am requesting an approval to enroll in CS123"
             }
-        self.assertEqual(self.notif_service1.send_notification(), expected)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert notif_service.send_notification() == expected
