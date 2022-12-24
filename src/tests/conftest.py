823674e66e11d5dfb735b197b4256c66c78a5254
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-12-24 11:47:15 +0900

    add MongoDB setup & teardown to test config

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -6,6 +6,7 @@ Fixtures defined here can be used in tests without needing to explicitly import 
 """
 
 import pytest
+import json
 import os, sys, inspect
 ##
 curr_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -14,17 +15,19 @@ sys.path.append(parent_dir)
 ##
 
 from regie_pkg.mysql_connect import MySQLConnect
+from regie_pkg.get_mongo_client import get_mongo_client
+from regie_pkg.mongodb import MongoDB
 
 @pytest.fixture(scope="function", autouse=True) # run automatically for every test function
-def test_db():
+def test_mysql():
     """
-    "Ensure that the test database is set up and torn down for each test."
+    "Ensure that the test MySQL database is set up and torn down for each test."
 
     This fixture creates a new MySQL database for testing named "REGIE_db_test",
-    creates tables in the database using the SQL statements in 'sql_files/create_table.sql',
-    and populates the tables with test data using the SQL statements in 'tests/populate_test.sql'.
-    The fixture also resets the MySQLConnect singleton to use the production database
-    "REGIE_db" after the test is finished.
+    creates tables in the database using the SQL statements in 'database_files/create_table.sql',
+    and populates the tables with test data using the SQL statements in
+    'test_data/mysql_test_data.sql'. The fixture also resets the MySQLConnect singleton
+    to use the production database "REGIE_db" after the test is finished.
     """
     MySQLConnect.set_database(None)
 
@@ -35,11 +38,11 @@ def test_db():
     MySQLConnect.set_database(test_db_name)
 
     # create tables
-    with open('../../sql_files/create_table.sql', encoding='utf-8') as f:
+    with open('../../database_files/create_table.sql', encoding='utf-8') as f:
         mysql_conn.execute_query(f.read(), multi=True)
 
     # populate tables with test data
-    with open('populate_test.sql', encoding='utf-8') as f:
+    with open('./test_data/mysql_test_data.sql', encoding='utf-8') as f:
         mysql_conn.execute_query(f.read(), multi=True, commit=True)
 
     yield
@@ -48,3 +51,30 @@ def test_db():
     mysql_conn.execute_query(f'DROP DATABASE IF EXISTS {test_db_name};')
     prod_db_name = "REGIE_db"
     MySQLConnect.set_database(prod_db_name)
+
+@pytest.fixture(scope="function", autouse=True) # run automatically for every test function
+def test_mongodb():
+    """
+    "Ensure that the test MongoDB database is set up and torn down for each test."
+
+    This fixture creates a new MongoDB database for testing named ""credential_db_test"",
+    creates a collection "student", and populates the collection with test data from
+    test_data/mongodb_test_data.json.
+    The fixture also resets MongoDB class to reference the production db "credential_db" after
+    each test.
+    """
+    mongo_client = get_mongo_client()
+    test_db_name = "credential_db_test"
+    credential_db = mongo_client[test_db_name]
+    student = credential_db['student'] # student collection
+    with open('./test_data/mongodb_test_data.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        student.insert_many(data)
+
+    MongoDB.set_database(test_db_name)
+
+    yield
+
+    mongo_client.drop_database(test_db_name)
+    prod_db_name = "credential_db"
+    MongoDB.set_database(prod_db_name)
