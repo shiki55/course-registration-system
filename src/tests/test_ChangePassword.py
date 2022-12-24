3d03f85bc70b10fa944742a7397ea6e0de1e651c
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-12-24 12:00:54 +0900

    clean up change password test

--- src/tests/test_ChangePassword.py
+++ src/tests/test_ChangePassword.py
@@ -1,6 +1,5 @@
 import os, sys, inspect
-from unittest import TestCase, mock
-import unittest
+from unittest import mock
 ##
 curr_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(curr_dir)
@@ -10,29 +9,23 @@ from regie_pkg.student import Student
 from regie_pkg.get_mongo_client import get_mongo_client
 
 
-class TestChangePassword(TestCase):
-    def setUp(self): # ran before each method w/ prefix "test_"
-        self.mongo_client = get_mongo_client()
-        self.student_id = '100'
-        self.initial_password = 'password123'
-        self.mongo_client.password_db.student.insert_one({self.student_id: self.initial_password})
-        self.student_obj = Student(id=int(self.student_id))
-        return
+class TestChangePassword:
+    """A class for testing the change_password method of abstract class REGIEPerson"""
 
-    def tearDown(self):
-        res = self.mongo_client.password_db.student.find_one({self.student_id: {'$exists': 'true'}})
-        self.mongo_client.password_db.student.delete_one(res)
-        return
+    mongo_client = get_mongo_client()
+    student = Student(id=1)
 
     @mock.patch('regie_pkg.regie_person.input', create=True)
-    def test_change_password_student(self, mock_input):
+    def test_change_password_student_success(self, mock_input):
+        """Test the change_password method with a successful password change."""
+        
+        original_password = 'pass123'
         new_password = 'new_password'
-        mock_input.side_effect = [self.initial_password, new_password] # mock inputs
-        self.student_obj.change_password()
-        res = self.mongo_client.password_db.student.find_one({self.student_id: {'$exists': 'true'}})
-        actual_password = res[self.student_id]
-        self.assertEqual(actual_password, new_password)
+        mock_input.side_effect = [original_password, new_password] # mock input
 
-
-if __name__ == '__main__':
-    unittest.main()
+        self.student.change_password()
+        student_id = str(self.student.id)
+        result = self.mongo_client.credential_db_test.student.find_one({student_id: {'$exists': 'true'}})
+        print(result)
+        actual_password = result[student_id]
+        assert actual_password == new_password
