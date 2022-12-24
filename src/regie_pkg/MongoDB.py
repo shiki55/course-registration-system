2bb2721f72964c671a1e26388fed36a86cccf70e
Toshiki Matsuya <toshikimatsuya@gmail.com>
2022-12-24 11:20:03 +0900

    add class method to change referenced mongodb database name at runtime

--- src/regie_pkg/MongoDB.py
+++ src/regie_pkg/MongoDB.py
@@ -1,23 +1,30 @@
-"""This module provides a class for interacting with a MongoDB database."""
+"""This module provides a class for interacting with the MongoDB database."""
 
 from regie_pkg.get_mongo_client import get_mongo_client
 
 class MongoDB:
     """Serves as the interface through which domain objects communicate with MongoDB"""
-    __mongo_client = get_mongo_client()
+    mongo_client = get_mongo_client()
+    database = "credential_db"
+
+    @classmethod
+    def set_database(cls, db_name: str) -> None:
+        """Set the name of the database to connect to."""
+
+        cls.database = db_name
 
     def verify(self, id, curr_password, collection_name) -> bool:
         """Verify whether the given ID and password are correct."""
-        password_db = self.__mongo_client['password_db']
-        res = password_db[collection_name].find_one({str(id): curr_password})
+        credential_db = self.mongo_client[self.database]
+        res = credential_db[collection_name].find_one({str(id): curr_password})
         if res is None:
             return False
         return True
 
     def change_password(self, id, new_password, collection_name) -> None:
         """Update the password for the given ID in the database."""
-        password_db = self.__mongo_client['password_db']
-        res = password_db[collection_name].find_one({str(id): {'$exists': 'true'}})
+        credential_db = self.mongo_client[self.database]
+        res = credential_db[collection_name].find_one({str(id): {'$exists': 'true'}})
         mongo_id = res['_id'] # id of post
-        password_db[collection_name].update_one({"_id": mongo_id}, {"$set": {str(id): new_password}}, upsert=False) # update post
+        credential_db[collection_name].update_one({"_id": mongo_id}, {"$set": {str(id): new_password}}, upsert=False) # update post
         return
